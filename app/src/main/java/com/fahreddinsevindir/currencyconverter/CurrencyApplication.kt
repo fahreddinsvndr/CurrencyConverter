83009c0e09e9acbf64864ff00c16dc2bc24e7440
FahreddinSevindir <fahreddinsevindir@gmail.com>
2021-01-21 16:53:18 +0300

    Dagger-Hilt Setup and User Permission

---
+++ app/src/main/java/com/fahreddinsevindir/currencyconverter/CurrencyApplication.kt
@@ -0,0 +1,7 @@
+package com.fahreddinsevindir.currencyconverter
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class CurrencyApplication: Application()
